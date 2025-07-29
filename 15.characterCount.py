d462006bb632c253029d291481218159534da218
deborabastos <deborabastos14@gmail.com>
2025-07-29 17:45:08 -0300

    add comments to 15

--- 15.characterCount.py
+++ 15.characterCount.py
@@ -3,10 +3,12 @@ import pprint
 # This program will count how many times each letter appears in the message
 
 message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
-count = {} # 'character': value => ex: 'r': 5
+count = {} # count is a dictionary with key:value => 'character': value => ex: 'r': 5
 
 for character in message.upper():
 	count.setdefault(character, 0)
 	count[character] = count[character] + 1
 
+#pprint = pretty print: better outlook
 pprint.pprint(count) 
+
