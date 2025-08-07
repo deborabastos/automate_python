9631a734ab712a1e139551b438a5c9e528336025
deborabastos <deborabastos14@gmail.com>
2025-08-07 14:00:42 -0300

    add comments to 17

--- 17.advance_str.py
+++ 17.advance_str.py
@@ -71,37 +71,38 @@ print('Hello world'.endswith('world')) # True
 print('Hello world'.endswith('w')) # False
 
 # join() useful to join a list into a single string
-print(','.join(['cat', 'rat', 'bat']))
-print('-'.join(['cat', 'rat', 'bat']))
-print(' '.join(['cat', 'rat', 'bat']))
-print(''.join(['cat', 'rat', 'bat']))
+print(','.join(['cat', 'rat', 'bat'])) #cat,rat,bat
+print('-'.join(['cat', 'rat', 'bat'])) #cat-rat-bat
+print(' '.join(['cat', 'rat', 'bat'])) #cat rat bat
+print(''.join(['cat', 'rat', 'bat'])) #catratbat
 
 # split() takes a string and return a list. By standard space is used to split, but you can change it by adding a argumment
-print('My name is Debora'.split())
-print('My name is Debora'.split('m'))
-print('My,name,is,Debora'.split(','))
+print('My name is Debora'.split()) #['My', 'name', 'is', 'Debora']
+print('My name is Debora'.split('m')) #['My na', 'e is Debora']
+print('My,name,is,Debora'.split(',')) #['My', 'name', 'is', 'Debora']
 
 #ljust() and rjust() adds a padding after or before the string and fill it it with second argument
-print('Hello'.ljust(20))
-print('Hello'.rjust(20))
-print('Hello'.ljust(20, '-'))
-print('Hello'.rjust(20, '*'))
+print('Hello'.ljust(20)) #"Hello               "
+print('Hello'.rjust(20)) #"               Hello"
+print('Hello'.ljust(20, '-')) #Hello---------------
+print('Hello'.rjust(20, '*')) #***************Hello
 
 #center() adds padding to centralize the string and fill it it with second argument
-print('Hello'.center(20))
-print('Hello'.center(20, '-'))
-print('Hello'.center(20, '*'))
+print('Hello'.center(20)) #'       Hello        '
+print('Hello'.center(20, '-')) #-------Hello--------
+print('Hello'.center(20, '*')) #*******Hello********
 
 #strip(), rstrip(), lstrip() removes whitespace temporarily (it doesnt substitute the original variable)
-print('SpamSpamBaconSpamEggsSpamSpam'.strip('ampS')) #order doesnt matter
+print('SpamSpamBaconSpamEggsSpamSpam'.strip('ampS')) #order doesnt matter # BaconSpamEggs
 
 #replace()
-print('Hello There! Hey You!'.replace('H', 'h'))
+print('Hello There! Hey You!'.replace('H', 'h')) #hello There! hey You!
 
 import pyperclip
 
 pyperclip.copy('Hello!!!!!!!!') # sends 'Hello!!!!!!!! to transfer area
 print(pyperclip.paste()) # gets 'Hello!!!!!!!! from transfer area
+
 
 # String FORMATTING
 name = 'Alice'
@@ -110,11 +111,11 @@ time = '6 pm'
 food = 'turnips'
 
 str1 = 'Hello ' + name + ', you are invited to a party at ' + place + ' at ' + time +'. Please bring ' + food + '.'
-print(str1)
+print(str1) # Hello Alice, you are invited to a party at Main Street at 6 pm. Please bring turnips.
 
 # Using place holder
 str2 = 'Hello %s, you are invited to a party at %s at %s. Please bring %s.' % (name, place, time, food)
-print(str2)
+print(str2) # Hello Alice, you are invited to a party at Main Street at 6 pm. Please bring turnips.
 
 
 
